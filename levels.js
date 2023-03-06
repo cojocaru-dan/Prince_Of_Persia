f2909d9b7693848edcf385add6f5dc496d816d05
Laura <lauraandradac15@gmail.com>
2023-03-06 13:59:00 +0200

    level6

--- levels.js
+++ levels.js
@@ -1,4 +1,58 @@
 
+function getStairsMovementDirection(stairNumber, isClimbingStairs) {
+  if (isClimbingStairs === false && stairNumber % 2 === 0) {
+    return "down"; 
+  } else if (isClimbingStairs === false && stairNumber % 2 === 1){
+    return "right";
+  } else if(isClimbingStairs === true && stairNumber % 2 === 0) {
+    return "up"; 
+  } else if (isClimbingStairs === true && stairNumber % 2 === 1){
+    return "right";
+  }
+};
+
+function getZigZagMovementDirection(){
+};
+
+function manuallyControl() {
+};
+
+//Potion functions 
+
+function givePotion2Answer(){
+};
+function givePotion3Answer(){
+};
+function givePotion4Answer(){
+};
+function givePotion5Answer(){
+};
+function givePotion6Answer(){
+};
+function givePotion7Answer(){
+};
+function givePotion8Answer(){
+};
+function givePotion9Answer(){
+};
+function givePotion10Answer(){
+};
+function givePotion11Answer(){
+};
+function givePotion12Answer(){
+};
+
+// Potion functions ^
+
+
+function level7Move() {
+};
+
+function hasMovedToTile(){
+};
+
+function level8Move() {
+};
 
 // DON'T MODIFY THE CODE BELOW THIS LINE
 
